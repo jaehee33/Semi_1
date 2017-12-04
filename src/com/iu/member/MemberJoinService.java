0860bebccfbaa2f092e9dc3a3953cfc483d8f601
jaehee33 <cjhjw@hanmail.net>
2017-12-04 19:45:49 +0900

    m

--- src/com/iu/member/MemberJoinService.java
+++ src/com/iu/member/MemberJoinService.java
@@ -16,7 +16,7 @@ public class MemberJoinService implements Action {
 		ActionForward actionForward = new ActionForward();
 		String method=request.getMethod();
 		if(method.equals("POST")) {
-			
+
 			String job = request.getParameter("job");
 			if(job.equals("C")) {
 				MemberDTO memberDTO = new MemberDTO();
@@ -26,21 +26,26 @@ public class MemberJoinService implements Action {
 				memberDTO.setBirth(Date.valueOf(request.getParameter("birth")));
 				memberDTO.setPhone(request.getParameter("phone"));
 				memberDTO.setJob(request.getParameter("job"));
-				
+
 				MemberDAO memberDAO = new MemberDAO();
-		
+
 				int result=0;
 				try {
 					result=memberDAO.join(memberDTO);
-	
+
 				} catch (Exception e) {
 				}
-				actionForward.setCheck(true);
+				
 				if(result>0) {
 					actionForward.setCheck(false);
 					actionForward.setPath("../index.jsp");		
+				}else {
+					request.setAttribute("message", "fail");
+					request.setAttribute("path", "./memberJoin.member");
+					actionForward.setCheck(true);
+					actionForward.setPath("../WEB-INF/view/common/result.jsp");
 				}
-				
+
 			}else {
 				StoreJoinService storeJoinService = new StoreJoinService();
 				actionForward=storeJoinService.doProcess(request, response);
