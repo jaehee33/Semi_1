4ca6a551821380dbf48243107cdcf686789e7512
sjham <sjham7133@gmail.com>
2017-12-06 20:57:06 +0900

    commit

---
+++ src/com/iu/book/BookDAO.java
@@ -0,0 +1,23 @@
+package com.iu.book;
+
+import java.util.ArrayList;
+import com.iu.store.StoreDTO;
+
+public class BookDAO {
+
+	public int insert(BookDTO bookDTO) throws Exception{
+		return 0;
+	}
+	
+	public BookDTO selectOne(String id) throws Exception{
+		return null;
+	}
+	
+	public ArrayList<BookDTO> selectList(StoreDTO storeDTO) throws Exception{
+		return null;
+	}
+	
+	public int delete(String id) throws Exception{
+		return 0;
+	}
+}
---
+++ src/com/iu/book/BookViewService.java
@@ -0,0 +1,17 @@
+package com.iu.book;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.iu.action.Action;
+import com.iu.action.ActionForward;
+
+public class BookViewService implements Action {
+
+	@Override
+	public ActionForward doProcess(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
