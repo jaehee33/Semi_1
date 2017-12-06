2ee39ca62823771f65cd4a1d65f96e18f05b943f
bomblike611 <rhtn611@naver.com>
2017-12-06 21:27:14 +0900

    kindstart
    카인드 시작!

--- src/com/iu/kind/KindDAO.java
+++ src/com/iu/kind/KindDAO.java
@@ -1,15 +1,26 @@
 package com.iu.kind;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 import com.iu.store.StoreDTO;
+import com.iu.util.DBConnector;
 import com.iu.util.MakeRow;
 
 public class KindDAO {
 
 	
 	public int insert(KindDTO kindDTO) throws Exception{
-		return 0;
+		Connection con=DBConnector.getConnect();
+		String sql="insert into kind values (?,?,?)";
+		PreparedStatement st=con.prepareStatement(sql);
+		st.setString(1, kindDTO.getKind());
+		st.setInt(2, kindDTO.getPrice());
+		st.setString(3, kindDTO.getStore());
+		int result=st.executeUpdate();
+		DBConnector.disConnect(st, con);
+		return result;
 	}
 	
 	public ArrayList<KindDTO> selectList(MakeRow makeRow) throws Exception{
