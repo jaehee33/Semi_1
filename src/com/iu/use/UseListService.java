5cd641babf75214ed798787234690df31ba6a0ed
sjham <sjham7133@gmail.com>
2017-12-11 21:21:57 +0900

    commit

--- src/com/iu/use/UseListService.java
+++ src/com/iu/use/UseListService.java
@@ -1,24 +1,13 @@
 package com.iu.use;
 
-<<<<<<< HEAD
 import java.util.List;
-=======
-import java.util.ArrayList;
->>>>>>> jj
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.iu.action.Action;
 import com.iu.action.ActionForward;
-<<<<<<< HEAD:src/com/iu/useList/UseListService.java
-import com.iu.book.BookDTO;
-
-=======
 import com.iu.member.MemberDTO;
->>>>>>> sj_book:src/com/iu/use/UseListService.java
 import com.iu.util.MakePage;
-import com.iu.util.MakeRow;
 import com.iu.util.Pageing;
 
 public class UseListService implements Action {
@@ -31,16 +20,6 @@ public class UseListService implements Action {
 		
 		try {
 			curPage=Integer.parseInt(request.getParameter("curPage"));
-<<<<<<< HEAD:src/com/iu/useList/UseListService.java
-		}catch (Exception e){
-			}
-		MakeRow makeRow = new MakeRow();
-		makeRow.setKind(request.getParameter("kind"));
-		makeRow.setSearch(request.getParameter("search"));
-
-		BookDTO bookDTO = (BookDTO)request.getSession().getAttribute("list");
-		UseListDAO useListDAO = new UseListDAO();
-=======
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -49,32 +28,14 @@ public class UseListService implements Action {
 		useMakeRow.setSearch(request.getParameter("search"));
 		
 		MemberDTO memberDTO=(MemberDTO)request.getSession().getAttribute("member");
-		UseDAO useDAO = new UseDAO();
->>>>>>> sj_book:src/com/iu/use/UseListService.java
-		
+		UseDAO useDAO = new UseDAO();		
 
 		int totalCount;
 		try {
 			totalCount=useDAO.getTotalCount(useMakeRow);
 			MakePage makePage = new MakePage(curPage, totalCount);
-<<<<<<< HEAD:src/com/iu/useList/UseListService.java
-			makeRow = makePage.getMakeRow(makeRow);
-
-			
-<<<<<<< HEAD
-			List<UseListDTO> ar = useListDAO.selectList(makeRow); //db조회
-			List<BookDTO> ar2 = bookDAO.selectList(memberDTO);
-			System.out.println(ar);
-			System.out.println(ar2);
-=======
-			ArrayList<UseListDTO> ar = useListDAO.selectList2(bookDTO);
-
->>>>>>> jj
-			//pageing
-=======
 			useMakeRow = (UseMakeRow) makePage.getMakeRow(useMakeRow);
 			List<UseDTO> ar = useDAO.selectList(useMakeRow,memberDTO.getId());
->>>>>>> sj_book:src/com/iu/use/UseListService.java
 			Pageing pageing = makePage.pageing();
 			
 			request.setAttribute("list", ar);
