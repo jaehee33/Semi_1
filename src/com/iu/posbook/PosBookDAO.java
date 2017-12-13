c68fb4a84a05d8c30e2c9ad33e76e04adbe48dec
bomblike611 <rhtn611@naver.com>
2017-12-13 21:27:06 +0900

    save

--- src/com/iu/posbook/PosBookDAO.java
+++ src/com/iu/posbook/PosBookDAO.java
@@ -2,6 +2,9 @@ package com.iu.posbook;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.iu.util.DBConnector;
 
@@ -23,6 +26,32 @@ public class PosBookDAO {
 		DBConnector.disConnect(st, con);
 		
 		return result;
+	}
+	
+	public List<PosBookDTO> selectList(String store) throws Exception{
+		Connection con=DBConnector.getConnect();
+		String sql="select * from posbook where store=?";
+		PreparedStatement st=con.prepareStatement(sql);
+		st.setString(1, store);
+		ResultSet rs=st.executeQuery();
+		List<PosBookDTO> ar=new ArrayList<>();
+		while (rs.next()) {
+			PosBookDTO posBookDTO=new PosBookDTO();
+			posBookDTO.setNum(rs.getInt("num"));
+			posBookDTO.setId(rs.getString("id"));
+			posBookDTO.setName(rs.getString("name"));
+			posBookDTO.setPhone(rs.getString("phone"));
+			posBookDTO.setBk_date(rs.getDate("bk_date"));
+			posBookDTO.setStore(rs.getString("store"));
+			posBookDTO.setStyle(rs.getString("style"));
+			posBookDTO.setPrice(rs.getInt("price"));
+			posBookDTO.setCoupon(rs.getString("coupon"));
+			posBookDTO.setTime(rs.getString("time"));	
+			posBookDTO.setState(rs.getString("state"));
+			ar.add(posBookDTO);
+		}
+		DBConnector.disConnect(rs, st, con);
+		return ar;
 	}
 	
 }
