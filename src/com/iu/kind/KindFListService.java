11be06d766d969d2ffe1115c0ca5570b413fba13
bomblike611 <rhtn611@naver.com>
2017-12-14 17:19:38 +0900

    okok
    hello

--- src/com/iu/kind/KindFListService.java
+++ src/com/iu/kind/KindFListService.java
@@ -9,36 +9,21 @@ import com.iu.action.Action;
 import com.iu.action.ActionForward;
 import com.iu.files.FilesDAO;
 import com.iu.files.FilesDTO;
-import com.iu.member.MemberDTO;
 import com.iu.store.StoreDAO;
 import com.iu.store.StoreDTO;
 
-public class KindCListService implements Action {
+public class KindFListService implements Action {
 
 	@Override
 	public ActionForward doProcess(HttpServletRequest request, HttpServletResponse response) {
 		ActionForward actionForward=new ActionForward();
-		String id="";
-		try {
-			id=request.getParameter("id");
-		}catch (Exception e) {
-			// TODO: handle exception
-		}
-		StoreDAO storeDAO=new StoreDAO();
-		StoreDTO storeDTO=null;
-		try {
-			storeDTO = storeDAO.selectOne(id);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		FilesDAO filesDAO=new FilesDAO();
 		KindDAO kindDAO=new KindDAO();
 		ArrayList<KindDTO> ar= new ArrayList<>();
 		ArrayList<FilesDTO> ar2=new ArrayList<>();
 		try {
 			ar2=filesDAO.selectList();
-			ar=kindDAO.selectList(storeDTO.getStore());
+			ar=kindDAO.allselectList();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -47,7 +32,6 @@ public class KindCListService implements Action {
 		if(ar!=null) {
 			request.setAttribute("filelist", ar2);
 			request.setAttribute("kindlist", ar);
-			request.setAttribute("store", storeDTO);
 			actionForward.setCheck(true);
 			actionForward.setPath("../WEB-INF/view/kind/kindStoreList.jsp");
 		}else {
