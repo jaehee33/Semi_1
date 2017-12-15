e02b23be15f4c24d1885e3a119c6da10d5b3e899
sjham <sjham7133@gmail.com>
2017-12-15 19:42:20 +0900

    예약
    예약 시 로그인 안된경우 로그인페이지로 이동

--- src/com/iu/use/UseWriteService.java
+++ src/com/iu/use/UseWriteService.java
@@ -16,68 +16,76 @@ public class UseWriteService implements Action {
 	@Override
 	public ActionForward doProcess(HttpServletRequest request, HttpServletResponse response) {
 		ActionForward actionForward = new ActionForward();
+
 		MemberDTO memberDTO = (MemberDTO)request.getSession().getAttribute("member");
-		String method=request.getMethod();
-		
-		String store=request.getParameter("store");
-		String style=request.getParameter("style");
-		int price=0;
-		int num=0;
-		PosDAO posDAO=new PosDAO();
-		UseDAO useDAO = new UseDAO();
-		try {
-			price=Integer.parseInt(request.getParameter("price"));	
-			num=useDAO.getNum();
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-		if(method=="POST") {
-			UseDTO useDTO=new UseDTO();
-			useDTO.setNum(num);
-			useDTO.setId(memberDTO.getId());
-			useDTO.setName(memberDTO.getName());
-			useDTO.setPhone(memberDTO.getPhone());
-			useDTO.setBk_date(Date.valueOf(request.getParameter("bk_date")));
-			useDTO.setStore(store);
-			useDTO.setStyle(style);
-			useDTO.setPrice(price);
-			useDTO.setCoupon(request.getParameter("coupon"));
-			useDTO.setTime(request.getParameter("time"));
-			
-			PosDTO posDTO = new PosDTO();
-			posDTO.setNum(num);
-			posDTO.setPos_import(price);
-			posDTO.setKind(style);
-			posDTO.setStore(store);
-			posDTO.setState("예약");
-			
-		
-			int useresult=0;
-			int posresult=0;
-			
+		if(memberDTO != null) {
+			String method=request.getMethod();
+
+			String store=request.getParameter("store");
+			String style=request.getParameter("style");
+			int price=0;
+			int num=0;
+			PosDAO posDAO=new PosDAO();
+			UseDAO useDAO = new UseDAO();
 			try {
-				useresult=useDAO.insert(useDTO);
-				posresult=posDAO.insert(posDTO);
+				price=Integer.parseInt(request.getParameter("price"));	
+				num=useDAO.getNum();
 			} catch (Exception e) {
 				// TODO: handle exception
-				e.printStackTrace();
 			}
-			if(useresult>0 && posresult>0) {
-				actionForward.setCheck(false);
-				actionForward.setPath("./useList.use");		
+
+			if(method=="POST") {
+				UseDTO useDTO=new UseDTO();
+				useDTO.setNum(num);
+				useDTO.setId(memberDTO.getId());
+				useDTO.setName(memberDTO.getName());
+				useDTO.setPhone(memberDTO.getPhone());
+				useDTO.setBk_date(Date.valueOf(request.getParameter("bk_date")));
+				useDTO.setStore(store);
+				useDTO.setStyle(style);
+				useDTO.setPrice(price);
+				useDTO.setCoupon(request.getParameter("coupon"));
+				useDTO.setTime(request.getParameter("time"));
+
+				PosDTO posDTO = new PosDTO();
+				posDTO.setNum(num);
+				posDTO.setPos_import(price);
+				posDTO.setKind(style);
+				posDTO.setStore(store);
+				posDTO.setState("예약");
+
+
+				int useresult=0;
+				int posresult=0;
+
+				try {
+					useresult=useDAO.insert(useDTO);
+					posresult=posDAO.insert(posDTO);
+				} catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				if(useresult>0 && posresult>0) {
+					actionForward.setCheck(false);
+					actionForward.setPath("./useList.use");		
+				}else {
+					request.setAttribute("message", "fail");
+					request.setAttribute("path", "../store/storeList.store");
+					actionForward.setCheck(true);
+					actionForward.setPath("../WEB-INF/view/common/result.jsp");
+				}
 			}else {
-				request.setAttribute("message", "fail");
-				request.setAttribute("path", "../store/storeList.store");
+				request.setAttribute("store", store);
+				request.setAttribute("style", style);
+				request.setAttribute("price", price);
 				actionForward.setCheck(true);
-				actionForward.setPath("../WEB-INF/view/common/result.jsp");
+				actionForward.setPath("../WEB-INF/view/use/useWrite.jsp");
 			}
 		}else {
-			request.setAttribute("store", store);
-			request.setAttribute("style", style);
-			request.setAttribute("price", price);
+			request.setAttribute("message", "로그인이 필요합니다");
+			request.setAttribute("path", "../member/memberLogin.member");
 			actionForward.setCheck(true);
-			actionForward.setPath("../WEB-INF/view/use/useWrite.jsp");
+			actionForward.setPath("../WEB-INF/view/common/result.jsp");
 		}
 		return actionForward;
 	}
